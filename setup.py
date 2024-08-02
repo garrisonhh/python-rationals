f21930e305d1afb0b3451c437f734bf4a6784b78
garrisonhh <garrisonhh@pm.me>
2024-08-02 10:23:38 -0400

    comparison operators

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rationals",
-    version="0.1.1",
+    version="0.1.2",
     packages=["rationals"],
     package_dir={"rationals": "rationals"},
     package_data={"rationals": ["**/*.so"]},
